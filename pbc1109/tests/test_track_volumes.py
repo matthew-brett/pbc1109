1cfd73c3ac449e5f77f8f85710d8f03a3f1c9e73
Matthew Brett <matthew.brett@gmail.com>
2009-10-20 12:41:52 -0500

    Committing as yet broken draft of cython track_volumes implementation

--- pbc1109/tests/test_track_volumes.py
+++ pbc1109/tests/test_track_volumes.py
@@ -12,8 +12,24 @@ from numpy.testing import assert_array_equal, assert_array_almost_equal
 import pbc1109.track_volumes as tv
 #import pbc1109.tvs as tv
 
+def tracks_to_expected(tracks, vol_dims):
+    expected = np.zeros(vol_dims, dtype=np.uint16)
+    for t in tracks:
+        ti = np.round(t).astype(np.int32)
+        for p in ti:
+            expected[tuple(p)] +=1
+    return expected
+
 
 def test_track_volumes():
+    vol_dims = (1, 2, 3)
+    tracks = ([[0, 0, 0],
+               [0, 1, 1]],)
+    tracks = [np.array(t) for t in tracks]
+    expected = tracks_to_expected(tracks, vol_dims)
+    tcs = tv.track_counts(tracks, vol_dims, [1,1,1])
+    yield assert_array_equal, tcs, expected
+
     vol_dims = (5, 10, 15)
     tracks = ([[0, 0.1, 0],
                [1, 1, 1],
@@ -22,13 +38,36 @@ def test_track_volumes():
                [1, 1, 1],
                [1, 2, 2]])
     tracks = [np.array(t) for t in tracks]
-    expected = np.zeros(vol_dims, dtype=np.uint16)
-    for t in tracks:
-        ti = np.round(t).astype(np.int32)
-        for p in ti:
-            expected[p] +=1
+    expected = tracks_to_expected(tracks, vol_dims)
     tcs = tv.track_counts(tracks, vol_dims, [1,1,1])
     yield assert_array_equal, tcs, expected
     
                
               
+def test_unique_elements():
+    vol_dims = np.array((5, 10, 15))
+    track = np.array([
+            [-1, 0, 0],
+            [0, 0, 0],
+            [1, 1, 1],
+            [1, 1, 1],
+            [2, 0, 0],
+            [2, 2, 2],
+            [6, 2, 2]])
+    n = track.shape[0]
+    expected_u_els = []
+    up_dims = np.tile(vol_dims, (n, 1))
+    for pno in (1, 2, 4, 5, 6): # unique point indices
+        t = track[pno]
+        t[t<0] = 0
+        too_high = t >= vol_dims
+        t[too_high] = vol_dims[too_high]
+        x, y, z = t
+        expected_u_els.append(
+            x +
+            y*vol_dims[0] +
+            z*vol_dims[0]*vol_dims[1])
+    expected_u_els.sort()
+    u_els = tv._unique_elements(track, vol_dims)
+    yield assert_array_equal, u_els, expected_u_els
+             
--- pbc1109/track_volumes.pyx
+++ pbc1109/track_volumes.pyx
@@ -1,6 +1,13 @@
-''' Counting incidence of tracks in voxels of volume '''
+''' A type of -*- python -*- file
 
+Counting incidence of tracks in voxels of volume
+
+'''
 import numpy as np
+cimport numpy as cnp
+
+cdef extern from "math.h":
+    double floor(double x)
 
 
 def track_counts(tracks, vol_dims, vox_sizes):
@@ -25,11 +32,58 @@ def track_counts(tracks, vol_dims, vox_sizes):
        that passed through voxel at coordinate x, y, z
     
     '''
-    tcs = np.zeros(vol_dims, dtype=np.uint16)
-    vox_sizes = np.array(vox_sizes, dtype=np.float)
-    for t in tracks:
-        ti = np.round(t / vox_sizes).astype(np.int32)
-        assert ti.dtype.kind == 'i'
-        for p in ti:
-            tcs[p] += 1
-    return tcs
+    n_voxels = np.prod(vol_dims)
+    cdef cnp.ndarray tcs = np.zeros((n_voxels,), dtype=np.int32)
+    cdef cnp.ndarray vd = np.array(vol_dims).astype(np.int32)
+    cdef cnp.ndarray vxs = np.array(vox_sizes).astype(np.float32)
+    cdef cnp.ndarray t
+    cdef cnp.ndarray in_pt
+    cdef cnp.ndarray ct_els
+    cdef int out_pt[3]
+    cdef int tno, pno, cno
+    cdef int xy = vd[0] * vd[1]
+    for tno from 0 <= tno < len(tracks):
+        in_inds = set()
+        t = tracks[tno].astype(np.float32)
+        for pno from 0 <= pno < t.shape[0]:
+            in_pt = t[pno]
+            for cno from 0 <=cno < 3:
+                v = <int>floor(in_pt[cno] * vxs[cno] + 0.5)
+                if v < 0:
+                    v = 0
+                elif v >= vd[cno]:
+                    v = vd[cno]
+                out_pt[cno] = v
+            el_no = (out_pt[0] * xy +
+                     out_pt[1] * vd[0] +
+                     out_pt[2])
+            if el_no in in_inds:
+                continue
+            in_inds.add(el_no)
+            tcs[el_no] += 1
+    return tcs.reshape(vol_dims)
+
+
+def _unique_elements(vtrack, dims):
+    ''' Convert voxel track coordinates to indices in flattened array
+
+    Parameters
+    ----------
+    vtrack : array shape (N, 3)
+       voxel xyz coordinates of points in track
+    dims : sequence
+       dimensions of 3D array to which `vtrack` coordinates refer
+
+    Returns
+    -------
+    unique_els : array shape (N,)
+       elements (indices into flattened array of shape `dims`) for each
+       point in `vtrack`.
+    '''
+    xd, yd, zd = dims
+    x, y, z = vtrack.T
+    np.clip(x, 0, xd, x)
+    np.clip(y, 0, yd, y)
+    np.clip(z, 0, zd, z)
+    el_nos = x + y*xd + z*xd*yd
+    return np.unique(el_nos)
